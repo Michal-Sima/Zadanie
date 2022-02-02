24608cbbf07a8afd5252f3ca8d7845b1f14dbf28
Michal Šima <michal.sima@student.tuke.sk>
2022-02-02 19:47:52 +0100

    Zadanie_5

--- Zadanie/Person.cs
+++ Zadanie/Person.cs
@@ -11,18 +11,15 @@ namespace Zadanie
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-
-        // metoda public string DisplayName() ma velmi podobnu funkcionalitu ako public void DisplayToConsoleDirectly(),
-        // preto ju netreba
-        /*public string DisplayName() // momentally not used
+        public string DisplayName() // momentally not used
         {
             return (FirstName + " " + LastName);
-        }*/
+        }
 
         public void DisplayToConsoleDirectly()
         {
             Console.WriteLine("This is print from under the hood: ");
-            Console.WriteLine(FirstName + " " + LastName);
+            Console.WriteLine(DisplayName());
             Console.WriteLine();
         }
 
